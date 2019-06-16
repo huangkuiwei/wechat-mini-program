d0975d315edc743e58d53bef78c73d2256494c73
huangkui <1017969390@qq.com>
2019-06-16 20:13:47 +0800

    小程序

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 App({
   onLaunch() {
-    // 弹出数据
-    // wx.setStorage({
-    // key: 'dialog',
-    // data: {
-    // 'complainDialog': false,
-    // 'ruleDialog': false
-    // }
-    // })
+
   },
   globalData: {
     // 本地 ip 地址
